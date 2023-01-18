36e173b8e716ea264c192e684006a7640eb63388
TheAdmin <didibleus2002@gmail.com>
2023-01-18 14:03:54 +0100

    MVC + Firebase.js + ajout oeil Login |OK|

--- Services/Auth/inscription.js
+++ Services/Auth/inscription.js
@@ -1,59 +1,5 @@
-// Import the functions you need from the SDKs you need
+import { Inscription } from "../../IServices/Firebase.js";
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-app.js";
-
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-analytics.js";
-
-import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signOut,
-} from "https://www.gstatic.com/firebasejs/9.12.1/firebase-auth.js";
-
-import {
-  getDatabase,
-  set,
-  ref,
-  update,
-} from "https://www.gstatic.com/firebasejs/9.12.1/firebase-database.js";
-
-// TODO: Add SDKs for Firebase products that you want to use
-
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
-const firebaseConfig = {
-  apiKey: "AIzaSyC9_gWkyQMS_gdVgN8_8WiMKcBfFWxs3UY",
-
-  authDomain: "projet-tikker.firebaseapp.com",
-
-  databaseURL:
-    "https://projet-tikker-default-rtdb.europe-west1.firebasedatabase.app",
-
-  projectId: "projet-tikker",
-
-  storageBucket: "projet-tikker.appspot.com",
-
-  messagingSenderId: "1028480647724",
-
-  appId: "1:1028480647724:web:cf7ab4bc919d46a1a80789",
-
-  measurementId: "G-GRJ9P5SSJR",
-};
-
-// Initialize Firebase
-
-const app = initializeApp(firebaseConfig);
-
-const auth = getAuth();
-
-const database = getDatabase(app);
-
-const analytics = getAnalytics(app);
 
 const EnterMail = document.getElementById("email");
 const EnterPre = document.getElementById("prenom");
@@ -61,10 +7,20 @@ const EnterNom = document.getElementById("nom");
 const EnterPass1 = document.getElementById("pwd");
 const EnterPass2 = document.getElementById("pwd2");
 
-const errorinput = () =>
-  document
-    .querySelectorAll("span")
-    .forEach((node) => (node.style.color = "#fc4103"));
+
+
+const showpwd = document.getElementById("showpwd");
+let showpwdIndex = 0;
+const showpwdImages = ["../../assets/icons/showpwdoff.svg", "../../assets/icons/showpwdon.svg"];
+const showpwdType = ["password", "text"];
+
+
+showpwd.addEventListener("click", function() {
+    showpwdIndex = (showpwdIndex + 1) % showpwdImages.length;
+    showpwd.src = showpwdImages[showpwdIndex];
+    EnterPass1.type = showpwdType[showpwdIndex];
+});
+
 
 function errorpassword() {
   EnterPass1.style.color = "red";
@@ -171,45 +127,5 @@ submitaccount.addEventListener("click", function () {
   }
 });
 
-function Inscription(email, prenom, nom, password) {
-  createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      // Inscription
-      const user = userCredential.user;
-      let CreaDate = new Date().toLocaleDateString("fr-FR");
-      // ...
-      set(ref(database, "users/" + user.uid), {
-        email: email,
-        prenom: prenom,
-        nom: nom,
-        password: password,
-        created: CreaDate,
-      })
-        .then(() => {
-          // Data saved successfully!
-          alert("Compte créer avec succès !");
-          Deconnexion();
-          window.location.href = "../Login";
-        })
-        .catch((error) => {
-          // The write failed...
-          alert(error);
-        });
-    })
-    .catch((error) => {
-      const errorMessage = "Des champs sont incorrectes.";
-      alert(errorMessage);
-      errorinput();
-    });
-}
 
-function Deconnexion() {
-  signOut(auth)
-    .then(() => {
-      // Sign-out successful.
-    })
-    .catch((error) => {
-      // An error happened.
-      alert(error);
-    });
-}
+
