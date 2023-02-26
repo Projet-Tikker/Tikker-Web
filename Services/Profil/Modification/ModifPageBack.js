c5be95a710715cd927b3c7aad3bf57bae99ac113
TheDi4nier <diego.pro.villanueva@gmail.com>
2023-02-26 18:42:45 +0100

    Amélioration Modif Page test hosting

--- Services/Profil/Modification/ModifPageBack.js
+++ Services/Profil/Modification/ModifPageBack.js
@@ -3,10 +3,10 @@ function changepp() {
   // var x = element.getElementsByTagName("img").item(0);
   var v = k.getAttribute("src");
   if (
-    (v == "../../../assets/users/user-admin.svg",
-    (v = "../../../assets/users/user-default.svg"))
+    (v == "/assets/users/user-admin.svg",
+    (v = "/assets/users/user-default.svg"))
   );
-  else v = "../../../assets/users/user-admin.svg";
+  else v = "/assets/users/user-admin.svg";
   k.setAttribute("src", v);
 }
 
@@ -15,10 +15,10 @@ function changepp1() {
   // var x = element.getElementsByTagName("img").item(0);
   var v = k.getAttribute("src");
   if (
-    (v == "../../../assets/users/user-default.svg",
-    (v = "../../../assets/users/user-admin.svg"))
+    (v == "/assets/users/user-default.svg",
+    (v = "/assets/users/user-admin.svg"))
   );
-  else v = "../../../assets/users/user-default.svg";
+  else v = "/assets/users/user-default.svg";
   k.setAttribute("src", v);
 }
 
@@ -27,14 +27,14 @@ function changepp2() {
   // var x = element.getElementsByTagName("img").item(0);
   var v = k.getAttribute("src");
   if (
-    (v == "../../../assets/users/user-admin.svg",
-    (v = "../../../assets/users/user-eye.svg"))
+    (v == "/assets/users/user-admin.svg",
+    (v = "/assets/users/user-eye.svg"))
   );
-  else v = "../../../assets/users/user-admin.svg";
+  else v = "/assets/users/user-admin.svg";
   if (
-    (v == "../../../assets/users/user-default.svg",
-    (v = "../../../assets/users/user-eye.svg"))
+    (v == "/assets/users/user-default.svg",
+    (v = "/assets/users/user-eye.svg"))
   );
-  else v = "../../../assets/users/user-default.svg";
+  else v = "/assets/users/user-default.svg";
   k.setAttribute("src", v);
 }
